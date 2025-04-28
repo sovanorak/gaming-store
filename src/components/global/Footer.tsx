5614f52c83e3d09aa2c9fe673f4149be61a403d3
Sovanorak Long <norakeusever@gmail.com>
2025-04-28 23:14:29 +0700

    set up project

---
+++ src/components/global/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="w-full">
+      <div className="max-w-screen-xl bg-slate-300 mx-auto">Footer</div>
+    </footer>
+  );
+}
---
+++ src/components/global/Navbar.tsx
@@ -0,0 +1,7 @@
+export function Navbar() {
+  return (
+    <nav className="bg-slate-300 w-full max-w-screen-xl mx-auto p-2">
+      Navbar
+    </nav>
+  );
+}
